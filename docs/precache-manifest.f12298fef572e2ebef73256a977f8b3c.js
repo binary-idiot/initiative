7eaab938a726d8bbc748a1bff7a20210a13fa590
Jonah Middleton <jonah.middleton@gmail.com>
2021-02-03 19:20:20 -0500

    Feat: Setup project for deployment to github pages

---
+++ docs/precache-manifest.f12298fef572e2ebef73256a977f8b3c.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "094343924f710a3a3b583a3ca85bb8a7",
+    "url": "/CNAME"
+  },
+  {
+    "revision": "3828886199cdd146e7e3",
+    "url": "/css/app.cf1c2ea1.css"
+  },
+  {
+    "revision": "d8e2c07f67f3de74989d2d5a0c0f3b0d",
+    "url": "/data/monsters/srd.json"
+  },
+  {
+    "revision": "f5a1c8b06b6d85ff55ed06b551362d72",
+    "url": "/data/spells/srd.json"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "41d92212cacbb00cb5eab9bbc90c170f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3828886199cdd146e7e3",
+    "url": "/js/app.5bf2962c.js"
+  },
+  {
+    "revision": "4e5f9bf49924c7f4e4f3",
+    "url": "/js/chunk-vendors.f0b1807f.js"
+  },
+  {
+    "revision": "8f4c3908afd3ee5dd848b693cb298069",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
