3192a4d37c86ddbd703b955b3ec9d79c97e7831c
Jonah Middleton <jonah.middleton@gmail.com>
2021-02-03 22:21:46 -0500

    Fix: Add rule to eslint to allow CRLF line endings

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,6 +13,7 @@ module.exports = {
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    "linebreak-style": ["error", process.env.NODE_ENV === 'prod' ? "unix" : "windows"],
   },
   overrides: [
     {
