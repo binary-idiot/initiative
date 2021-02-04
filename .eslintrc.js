2f6dd7af0ea0ca346268808c30d15f7297037889
Jonah Middleton <jonah.middleton@gmail.com>
2021-02-04 17:19:49 -0500

    Chore: Add eslint rules to allow tab indentation

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,6 +15,8 @@ module.exports = {
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     "linebreak-style": ["error", process.env.NODE_ENV === 'prod' ? "unix" : "windows"],
     "prefer-destructuring": ["error", {"object": true, "array": false}],
+    "indent": ["error", "tab"],
+    "no-tabs": ["error", {"allowIndentationTabs": true}]
   },
   overrides: [
     {
