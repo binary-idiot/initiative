ca0243a37164e83492f881e4e314f51a368bdd55
Jonah Middleton <jonah.middleton@gmail.com>
2021-02-04 04:33:07 -0500

    Fix: Add untracked files

---
+++ docs/precache-manifest.ad8845c0f38baf7bdb4040099eba8a2c.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "094343924f710a3a3b583a3ca85bb8a7",
+    "url": "/CNAME"
+  },
+  {
+    "revision": "741abc5858c5ff142a33",
+    "url": "/css/app.d239e347.css"
+  },
+  {
+    "revision": "9d5f2a9bbaae65b895a0799b16478112",
+    "url": "/data/monsters/srd.json"
+  },
+  {
+    "revision": "6d216dd84758ca1bd3d3a7990906e53b",
+    "url": "/data/sources.json"
+  },
+  {
+    "revision": "5c37a24e89903b1910deed5ed383a305",
+    "url": "/data/spells/srd.json"
+  },
+  {
+    "revision": "9c8bb32c66ef30d90f6138d895ffc7fd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "741abc5858c5ff142a33",
+    "url": "/js/app.2a5e8ef6.js"
+  },
+  {
+    "revision": "3d7730c2023a51df29f2",
+    "url": "/js/chunk-vendors.a4ed62da.js"
+  },
+  {
+    "revision": "09b0303bff60da46ce15a1d210ef3312",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/robots.txt"
+  }
+]);
