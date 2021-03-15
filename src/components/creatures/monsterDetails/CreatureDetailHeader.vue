b24b4dc9ffe5773425a9351427e98782cca38ee6
Jonah Middleton <jonah.middleton@gmail.com>
2021-03-15 17:05:20 -0400

    Feat: Move details header to separate component

---
+++ src/components/creatures/monsterDetails/CreatureDetailHeader.vue
@@ -0,0 +1,50 @@
+<template>
+	<div class="creature-detail-header">
+		<h2>{{ creature.name }}</h2>
+		<span>{{ creatureSize }} {{ creatureType }}</span>
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'CreatureDetailHeader',
+
+	props: {
+		creature: Object,
+	},
+
+	computed: {
+		creatureSize() {
+			switch (this.creature.size) {
+			case 'T':
+				return 'tiny';
+			case 'S':
+				return 'small';
+			case 'M':
+				return 'medium';
+			case 'L':
+				return 'large';
+			case 'H':
+				return 'huge';
+			case 'G':
+				return 'gigantic';
+			default:
+				return null;
+			}
+		},
+
+		creatureType() {
+			const { type } = this.creature;
+			if (typeof type === 'string') {
+				return type;
+			}
+
+			return `${type.type} (${type.tags}) `;
+		},
+	},
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- src/components/creatures/monsterDetails/CreatureDetails.vue
+++ src/components/creatures/monsterDetails/CreatureDetails.vue
@@ -1,46 +1,23 @@
 <template>
 	<div class="creature-details">
 		<template v-if="creature">
-			<h2>{{ creature.name }}</h2>
-			<span>{{ creatureSize }} {{ creatureType }}</span>
+			<CreatureDetailHeader :creature ="creature"></CreatureDetailHeader>
 		</template>
 	</div>
 </template>
 
 <script>
+import CreatureDetailHeader from './CreatureDetailHeader.vue';
+
 export default {
 	name: 'CreatureDetails',
+
+	components: {
+		CreatureDetailHeader,
+	},
+
 	props: {
 		creature: Object,
-	},
-	computed: {
-		creatureSize() {
-			switch (this.creature.size) {
-			case 'T':
-				return 'tiny';
-			case 'S':
-				return 'small';
-			case 'M':
-				return 'medium';
-			case 'L':
-				return 'large';
-			case 'H':
-				return 'huge';
-			case 'G':
-				return 'gigantic';
-			default:
-				return null;
-			}
-		},
-
-		creatureType() {
-			const { type } = this.creature;
-			if (typeof type === 'string') {
-				return type;
-			}
-
-			return `${type.type} (${type.tags}) `;
-		},
 	},
 };
 </script>
