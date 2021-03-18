776afdb46742246fab9941c568987bb66d2e6959
Jonah Middleton <jonah.middleton@gmail.com>
2021-03-18 03:32:56 -0400

    Feat: Change favicon

--- vue.config.js
+++ vue.config.js
@@ -1,14 +1,20 @@
 module.exports = {
 	outputDir: 'docs',
 	pwa: {
-		themeColor: '#209CEE',
-		msTileColor: '#209CEE',
+		assetsVersion: '2',
+		themeColor: '#ffffff',
+		msTileColor: '#2d89ef',
 		iconPaths: {
 			androidChrome192: 'img/icons/android-chrome-192x192.png',
 			androidChrome512: 'img/icons/android-chrome-512x512.png',
 			appleTouchIcon: 'img/icons/apple-touch-icon.png',
 			favicon16: 'img/icons/favicon-16x16.png',
 			favicon32: 'img/icons/favicon-32x32.png',
+			maskIcon: 'img/icons/safari-pinned-tab.svg',
+			msTileImage: 'img/icons/mstile-144x144.png',
+		},
+		manifestOptions: {
+			background_color: '#ffffff',
 		},
 		workboxOptions: {
 			exclude: ['CNAME']
