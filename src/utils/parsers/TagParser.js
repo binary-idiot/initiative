88d5bdb5477213348f75aa02d760bc74fb16d1d8
Jonah Middleton <jonah.middleton@gmail.com>
2021-03-24 17:23:29 -0400

    Feat: Add tags to TagParser

--- src/utils/parsers/TagParser.js
+++ src/utils/parsers/TagParser.js
@@ -4,7 +4,30 @@ class TagParser {
 		switch (tag) {
 		case 'item':
 		case 'spell':
+		case 'creature':
+		case 'condition':
+		case 'skill':
+		case 'i':
 			return `<em>${params[0]}</em>`;
+		case 'dice':
+		case 'damage':
+			return `<strong>${params[0]}</strong>`;
+		case 'dc':
+			return `<strong>DC: ${params[0]}</strong>`;
+		case 'atk': {
+			const weaponType = params[0].split(',')
+				.map((wt) => ((wt === 'mw') ? 'melee' : 'ranged'));
+			return `<em>${(weaponType.length === 2)
+				? `${weaponType[0]} or ${weaponType[1]}` : weaponType[0]} weapon attack</em>`;
+		}
+		case 'hit':
+			return `+${params[0]}`;
+		case 'h':
+			return '<em>Hit:</em>';
+		case 'recharge':
+			return `<strong>(Recharge ${(params[0]) ? `${params[0]}-6` : 6})</strong>`;
+		case 'chance':
+			return params[1];
 		default:
 			throw new Error(`Tag ${tag} not defined`);
 		}
