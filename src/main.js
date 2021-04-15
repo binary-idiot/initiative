6c28ff212fc31ffd8704d505b0db9856277be31c
Jonah Middleton <jonah.middleton@gmail.com>
2021-04-15 04:06:23 -0400

    Fix: Prevent from analytics from firing locally

--- src/main.js
+++ src/main.js
@@ -5,12 +5,19 @@ import './registerServiceWorker';
 import Functions from './utils/GlobalFunctions';
 import router from './router';
 
+let enableGA = true;
+
+if (window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1') {
+	enableGA = false;
+}
+
 const app = createApp(App)
 	.use(router)
 	.use(gtag, {
 		property: {
 			id: 'G-ZJ7V88XG65',
 		},
+		isEnabled: enableGA,
 	});
 
 app.config.globalProperties.$func = Functions;
