e3a0eef2fac5abcae1b7c990de445eb174354059
Jonah Middleton <jonah.middleton@gmail.com>
2021-04-19 21:30:23 -0400

    Feat: Highlight current page on tab menu

--- src/components/MenuBar.vue
+++ src/components/MenuBar.vue
@@ -1,19 +1,19 @@
 <template>
 	<nav class="menu-bar">
 		<router-link :to="{name: 'Creatures'}">
-			<div>
+			<div class="creature-btn menu-btn">
 				<IconBase :width="24" :height="24" icon-name="creatures"><IconCreature /></IconBase>
 				<span>Creatures</span>
 			</div>
 		</router-link>
 		<router-link :to="{name: 'Tracker'}">
-			<div>
+			<div class="tracker-btn menu-btn">
 				<IconBase :width="35" :height="35" icon-name="initiative"><IconSwords /></IconBase>
 				<span>Tracker</span>
 			</div>
 		</router-link>
 		<router-link :to="{name: 'Settings'}">
-			<div>
+			<div class="settings-btn menu-btn">
 				<IconBase :width="16" :height="16" icon-name="settings"><IconSettings /></IconBase>
 				<span>Settings</span>
 			</div>
@@ -35,6 +35,38 @@ export default {
 		IconSwords,
 		IconSettings,
 	},
+	methods: {
+		highlightMenuItem() {
+			document.getElementsByClassName('menu-btn')
+				.forEach((e) => {
+					e.classList.remove('active-page');
+				});
+
+			const { path } = this.$route;
+			const page = path.split('/')[1].toLowerCase();
+			let menuBtn;
+			switch (page) {
+			case 'creatures':
+				menuBtn = document.getElementsByClassName('creature-btn');
+				break;
+			case 'settings':
+				menuBtn = document.getElementsByClassName('settings-btn');
+				break;
+			default:
+				menuBtn = document.getElementsByClassName('tracker-btn');
+				break;
+			}
+
+			menuBtn.forEach((e) => {
+				e.classList.add('active-page');
+			});
+		},
+	},
+	watch: {
+		$route() {
+			this.highlightMenuItem();
+		},
+	},
 };
 </script>
 
@@ -52,17 +84,20 @@ export default {
 		flex-direction: row;
 		align-content: center;
 
+		background-color: $contrast-light;
+
 		@media screen and (min-width: 800px),
 			screen and (max-height: 400px){
 			flex-direction: column;
 			height: 100%;
 			width: 100px;
 		}
+
 		a{
-			color: $contrast-3;
+			color: $contrast-4;
 
 			&:hover{
-				color: $contrast-2;
+				color: $contrast-3;
 			}
 
 			margin: auto;
@@ -80,6 +115,8 @@ export default {
 			}
 		}
 
-		background-color: $contrast-light;
+		.active-page{
+			color: $contrast-2;
+		}
 	}
 </style>
