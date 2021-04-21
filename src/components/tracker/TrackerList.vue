1db0aee4e02e9616adf5cea0d100b56320981e59
Jonah Middleton <jonah.middleton@gmail.com>
2021-04-21 00:47:29 -0400

    Feat: Create entities store and corresponding view

---
+++ src/components/tracker/TrackerList.vue
@@ -0,0 +1,46 @@
+<template>
+	<div class="tracker-list">
+		<table>
+			<thead>
+				<tr>
+					<th>Order</th>
+					<th>Name</th>
+					<th>HP</th>
+				</tr>
+			</thead>
+			<tbody>
+				<tr v-for="(entity, index) in entities" :key="index">
+					<td>{{ entity.order }}</td>
+					<td>{{ entity.name }}</td>
+					<td>{{ entity.hp }}</td>
+				</tr>
+			</tbody>
+		</table>
+	</div>
+</template>
+
+<script>
+import { mapState } from 'vuex';
+
+export default {
+	name: 'TrackerList',
+	computed: {
+		...mapState({
+			entities: (state) => state.entities.sort((a, b) => a.order - b.order),
+		}),
+	},
+};
+</script>
+
+<style lang="scss" scoped>
+	.tracker-list{
+		table{
+			width: max-content;
+			margin: auto;
+
+			td{
+				padding: 2px 10px;
+			}
+		}
+	}
+</style>
--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,34 @@
 import { createStore } from 'vuex';
 
 export default createStore({
+	state: {
+		entities: [
+			{
+				order: 5, name: 'Paul Ross', hp: 50, isCreature: false,
+			},
+			{
+				order: 2, name: 'Raul Poss', hp: 100, isCreature: false,
+			},
+			{
+				order: 8, name: 'QT Pie', hp: 70, isCreature: false,
+			},
+		],
+	},
+	mutations: {
+		pushEntity(state, entity) {
+			state.entity.push(entity);
+		},
+	},
+	actions: {
+		addEntity({ commit }, e) {
+			const entity = {
+				order: e.order,
+				name: e.name,
+				hp: e.hp,
+				isCreature: e.isCreature,
+			};
 
+			commit('pushEntity', entity);
+		},
+	},
 });
