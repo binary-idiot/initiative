469cb541aad2f73355a75681e64e5a04127398e8
Jonah Middleton <jonah.middleton@gmail.com>
2021-04-24 15:47:14 -0400

    Feat: Implement adding and removeing entities from store

--- src/components/tracker/TrackerList.vue
+++ src/components/tracker/TrackerList.vue
@@ -13,21 +13,49 @@
 					<td>{{ entity.order }}</td>
 					<td>{{ entity.name }}</td>
 					<td>{{ entity.hp }}</td>
+					<td>
+						<button @click="removeEntityFromEncounter(index)">Delete</button>
+					</td>
 				</tr>
 			</tbody>
+			<tfoot>
+				<tr>
+					<td><input type="text" name="order" id="save-entity-order"></td>
+					<td><input type="text" name="name" id="save-entity-name"></td>
+					<td><input type="text" name="hp" id="save-entity-hp"></td>
+					<td><button @click="addEntityToEncounter">Add</button></td>
+				</tr>
+			</tfoot>
 		</table>
 	</div>
 </template>
 
 <script>
-import { mapState } from 'vuex';
+import { mapState, mapActions } from 'vuex';
 
 export default {
 	name: 'TrackerList',
 	computed: {
 		...mapState({
-			entities: (state) => state.entities.sort((a, b) => a.order - b.order),
+			entities: (state) => state.encounter.sort((a, b) => a.order - b.order),
 		}),
+	},
+	methods: {
+		addEntityToEncounter() {
+			const order = document.getElementById('save-entity-order').value;
+			const name = document.getElementById('save-entity-name').value;
+			const hp = document.getElementById('save-entity-hp').value;
+			this.saveEntity({
+				order, name, hp,
+			});
+		},
+		removeEntityFromEncounter(index) {
+			this.deleteEntity(index);
+		},
+		...mapActions([
+			'saveEntity',
+			'deleteEntity',
+		]),
 	},
 };
 </script>
